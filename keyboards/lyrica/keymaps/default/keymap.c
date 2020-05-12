80e8b0274be5f10ae4f88a6609b1b3fbc6e53234
jmding8 <44815547+jmding8@users.noreply.github.com>
2020-05-12 10:19:05 -0700

    Add Lyrica keyboard

---
+++ keyboards/lyrica/keymaps/default/keymap.c
@@ -0,0 +1,104 @@
+/* Copyright 2019 Thomas Baart <thomas@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+#define APL 0
+#define WIN 1
+#define NAV 2
+#define NUM 3
+#define ADJUST 4
+
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [APL] = LAYOUT(
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,          KC_U,      KC_I,    KC_O,     KC_P, KC_BSLS,
+       KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,          KC_J,      KC_K,    KC_L,  KC_SCLN, KC_QUOT,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,          KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT,
+                                 KC_LCTL, KC_LALT, KC_LCMD,    LT(NAV, KC_SPC), LT(NUM, KC_0), MO(ADJUST)
+    ),
+    [WIN] = LAYOUT(
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,          KC_U,      KC_I,    KC_O,     KC_P, KC_BSLS,
+       KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,          KC_J,      KC_K,    KC_L,  KC_SCLN, KC_QUOT,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,          KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT,
+                                 KC_LWIN, KC_LALT, KC_LCTL,    LT(NAV, KC_SPC), LT(NUM, KC_0), MO(ADJUST)
+    ),
+    [NAV] = LAYOUT(
+      _______, _______, _______, _______, _______, _______,    KC_CAPS, _______,   KC_UP, KC_PGUP, _______,  KC_DEL,
+      _______, _______, _______, _______, _______, _______,    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,  KC_ENT,
+      _______, _______, _______, _______, _______, _______,    _______, KC_PGDN, KC_HOME,  KC_END, _______, _______,
+                                 _______, _______, _______,    _______, _______, _______
+    ),
+    [NUM] = LAYOUT(
+      _______, _______, _______, _______, _______, _______,    _______,    KC_7,    KC_8,    KC_9, KC_MINS,  KC_EQL,
+      _______, _______, _______, _______, _______, _______,    _______,    KC_4,    KC_5,    KC_6, KC_LBRC, KC_RBRC,
+      _______, _______, _______, _______, _______, _______,    _______,    KC_1,    KC_2,    KC_3,    KC_0,  KC_GRV,
+                                 _______, _______, _______,    _______, _______, _______
+    ),
+    [ADJUST] = LAYOUT(
+      _______, _______, _______, _______, _______, _______,    _______, _______, KC_VOLU, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,    _______, KC_BRID, KC_VOLD, KC_BRIU, _______, _______,
+      _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______,
+                                 _______, _______, _______,    _______, _______, _______
+    )
+};
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+static uint8_t mode_switcher_state = 0;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // Mode switcher, RESET, WINDOWS and APPLE
+  if (keycode == KC_TAB || keycode == KC_BSLS) {
+    if (record->event.pressed) {
+      mode_switcher_state |= 0B10000000;
+    } else {
+      mode_switcher_state &= ~(0B10000000);
+    }
+  }
+
+  if (keycode == KC_T || keycode == KC_Y) {
+    if (record->event.pressed) {
+      mode_switcher_state |= 0B01000000;
+    } else {
+      mode_switcher_state &= ~(0B01000000);
+    }
+  }
+
+  if (mode_switcher_state == 0B11000000 && record->event.pressed) {
+    switch (keycode) {
+      case KC_R:
+        reset_keyboard();
+        break;
+      case KC_U:
+        reset_keyboard();
+        break;
+      case KC_A:
+          persistent_default_layer_set(APL);
+        layer_on(APL);
+        layer_off(WIN);
+        break;
+      case KC_W:
+        layer_off(APL);
+        layer_on(WIN);
+        break;
+    }
+  }
+
+  return true;
+}
--- keyboards/lyrica/readme.md
+++ keyboards/lyrica/readme.md
@@ -1,6 +1,6 @@
-# AppleSplit
+# Lyrica
 
-![AppleSplit](imgur.com image replace me!)
+![Lyrica](imgur.com image replace me!)
 
 A short description of the keyboard/project
 
@@ -10,7 +10,6 @@ Hardware Availability: links to where you can find this hardware
 
 Compile and flash example for this keyboard (after setting up your build environment, https://www.youtube.com/watch?v=u1lweFStx1U):
 
-    make lyrica:default:avrdude
+    make lyrica:default:avrdude-split-left
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
-
