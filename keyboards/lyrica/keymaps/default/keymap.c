86d15d571407b3902cd47de3c2ae3cc4a90944fa
jmding8 <44815547+jmding8@users.noreply.github.com>
2020-06-11 18:56:18 -0700

    move navnum left shift and capslock

--- keyboards/lyrica/keymaps/default/keymap.c
+++ keyboards/lyrica/keymaps/default/keymap.c
@@ -21,6 +21,11 @@
 #define FKEYS 3
 #define ADJUST 4
 
+// enum custom_keycodes {
+//   LS_CLCK = SAFE_RANGE,
+//   RS_CLCK,
+// };
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [APL] = LAYOUT(
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
@@ -35,13 +40,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  KC_LWIN, KC_LALT, KC_LCTL,    LT(NAVNUM, KC_SPC), LT(FKEYS, KC_ENT), MO(ADJUST)
     ),
     [NAVNUM] = LAYOUT(
-      _______,    KC_1,    KC_2,    KC_3,    KC_4, KC_LCBR,    KC_RCBR, _______,   KC_UP, KC_PGUP, _______,  KC_DEL,
-      _______,    KC_5,    KC_6,    KC_7,    KC_8, KC_LBRC,    KC_RBRC, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_BSLS,
-      KC_PSCR,    KC_9,    KC_0, KC_MINS,  KC_EQL,  KC_GRV,    _______, KC_PGDN, KC_HOME,  KC_END, _______, KC_RSFT,
+      _______,    KC_1,    KC_2,    KC_3,    KC_4, KC_LCBR,    KC_RCBR, _______,   KC_UP, KC_PGUP, KC_CLCK,  KC_DEL,
+      _______,    KC_5,    KC_6,    KC_7,    KC_8, KC_LBRC,    KC_RBRC, KC_LEFT, KC_DOWN, KC_RGHT, KC_CLCK, KC_BSLS,
+      KC_LSFT,    KC_9,    KC_0, KC_MINS,  KC_EQL,  KC_GRV,    _______, KC_PGDN, KC_HOME,  KC_END, KC_CLCK, KC_RSFT,
                                  _______, _______, _______,    _______, _______, _______
     ),
     [FKEYS] = LAYOUT(
-      _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4, _______,    _______, _______, _______, _______, _______, _______,
+      KC_PSCR,   KC_F1,   KC_F2,   KC_F3,   KC_F4, _______,    _______, _______, _______, _______, _______, _______,
       _______,   KC_F5,   KC_F6,   KC_F7,   KC_F8, _______,    _______, _______, _______, _______, _______, _______,
       _______,   KC_F1,  KC_F10,  KC_F11,  KC_F12, _______,    _______, _______, _______, _______, _______, _______,
                                  _______, _______, _______,    _______, _______, _______
@@ -60,7 +65,7 @@ void persistent_default_layer_set(uint16_t default_layer) {
 }
 
 static uint8_t mode_switcher_state = 0;
-static uint8_t capslock_state = 0;
+// static uint8_t capslock_state = 0;
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   // Mode switcher, RESET, WINDOWS and APPLE
   if (keycode == KC_TAB || keycode == KC_BSPC) {
@@ -100,28 +105,66 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
 
   // Double shift capslock
-  if (record->event.pressed) {
-    switch (keycode) {
-      case KC_LSFT:
-        capslock_state |= 0B10000000;
-        break;
-      case KC_RSFT:
-        capslock_state |= 0B01000000;
-        break;
-    }
-    if (capslock_state == 0B11000000) {
-      SEND_STRING(SS_TAP(X_CAPSLOCK));
-    }
-  } else {
-    switch (keycode) {
-      case KC_LSFT:
-        capslock_state &= ~(0B10000000);
-        break;
-      case KC_RSFT:
-        capslock_state &= ~(0B01000000);
-        break;
-    }
-  }
+  // if (record->event.pressed) {
+    // switch (keycode) {
+    //   case LS_CLCK:
+    //     capslock_state |= 0B10000000;
+    //     if (capslock_state == 0B11000000) {
+    //       SEND_STRING(SS_DOWN(X_CAPSLOCK));
+    //     } else {
+    //       SEND_STRING(SS_DOWN(X_LSFT));
+    //     }
+    //     break;
+    //   case RS_CLCK:
+    //     capslock_state |= 0B01000000;
+    //     if (capslock_state == 0B11000000) {
+    //       SEND_STRING(SS_DOWN(X_CAPSLOCK));
+    //     } else {
+    //       SEND_STRING(SS_DOWN(X_RSFT));
+    //     }
+    //     break;
+    //   }
+    // }
+
+    // switch (keycode) {
+    //   case KC_LSFT:
+    //     capslock_state |= 0B10000000;
+    //     break;
+    //   case KC_RSFT:
+    //     capslock_state |= 0B01000000;
+    //     break;
+    // }
+    // if (capslock_state == 0B11000000) {
+    //   SEND_STRING(SS_TAP(X_CAPSLOCK));
+    // }
+
+  // } else {
+  //   switch (keycode) {
+      // case LS_CLCK:
+      //   if (capslock_state == 0B11000000) {
+      //     SEND_STRING(SS_UP(X_CAPSLOCK)SS_UP(X_RSFT));
+      //   } else {
+      //     SEND_STRING(SS_UP(X_LSFT));
+      //   }
+      //   capslock_state &= ~(0B10000000);
+      //   break;
+      // case RS_CLCK:
+      //   if (capslock_state == 0B11000000) {
+      //     SEND_STRING(SS_UP(X_CAPSLOCK)SS_UP(X_LSFT));
+      //   } else {
+      //     SEND_STRING(SS_UP(X_RSFT));
+      //   }
+      //   capslock_state &= ~(0B01000000);
+      //   break;
+
+      // case KC_LSFT:
+      //   capslock_state &= ~(0B10000000);
+      //   break;
+      // case KC_RSFT:
+      //   capslock_state &= ~(0B01000000);
+      //   break;
+  //   }
+  // }
 
   return true;
 }
