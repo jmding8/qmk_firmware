53c2a8e9e11a93c00852e3b85c8000315ce02543
jmding8 <44815547+jmding8@users.noreply.github.com>
2020-06-11 18:57:51 -0700

    move capslock from navnum to fkeys layer

--- keyboards/lyrica/keymaps/default/keymap.c
+++ keyboards/lyrica/keymaps/default/keymap.c
@@ -40,15 +40,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  KC_LWIN, KC_LALT, KC_LCTL,    LT(NAVNUM, KC_SPC), LT(FKEYS, KC_ENT), MO(ADJUST)
     ),
     [NAVNUM] = LAYOUT(
-      _______,    KC_1,    KC_2,    KC_3,    KC_4, KC_LCBR,    KC_RCBR, _______,   KC_UP, KC_PGUP, KC_CLCK,  KC_DEL,
-      _______,    KC_5,    KC_6,    KC_7,    KC_8, KC_LBRC,    KC_RBRC, KC_LEFT, KC_DOWN, KC_RGHT, KC_CLCK, KC_BSLS,
-      KC_LSFT,    KC_9,    KC_0, KC_MINS,  KC_EQL,  KC_GRV,    _______, KC_PGDN, KC_HOME,  KC_END, KC_CLCK, KC_RSFT,
+      _______,    KC_1,    KC_2,    KC_3,    KC_4, KC_LCBR,    KC_RCBR, _______,   KC_UP, KC_PGUP, _______,  KC_DEL,
+      _______,    KC_5,    KC_6,    KC_7,    KC_8, KC_LBRC,    KC_RBRC, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_BSLS,
+      KC_LSFT,    KC_9,    KC_0, KC_MINS,  KC_EQL,  KC_GRV,    _______, KC_PGDN, KC_HOME,  KC_END, _______, KC_RSFT,
                                  _______, _______, _______,    _______, _______, _______
     ),
     [FKEYS] = LAYOUT(
-      KC_PSCR,   KC_F1,   KC_F2,   KC_F3,   KC_F4, _______,    _______, _______, _______, _______, _______, _______,
-      _______,   KC_F5,   KC_F6,   KC_F7,   KC_F8, _______,    _______, _______, _______, _______, _______, _______,
-      _______,   KC_F1,  KC_F10,  KC_F11,  KC_F12, _______,    _______, _______, _______, _______, _______, _______,
+      KC_PSCR,   KC_F1,   KC_F2,   KC_F3,   KC_F4, _______,    _______, _______, _______, _______, KC_CLCK, _______,
+      _______,   KC_F5,   KC_F6,   KC_F7,   KC_F8, _______,    _______, _______, _______, _______, KC_CLCK, _______,
+      _______,   KC_F1,  KC_F10,  KC_F11,  KC_F12, _______,    _______, _______, _______, _______, KC_CLCK, _______,
                                  _______, _______, _______,    _______, _______, _______
     ),
     [ADJUST] = LAYOUT(
